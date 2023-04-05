c3b64dc1f3b82cf031e3bacfb635df8a5e00a473
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-04-05 10:44:17 +0500

    init

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import add_phone,model_phones,search_name,upd_phone,sign_in,sign_up,add_product_cart
+from api.views import add_phone,model_phones,search_name,upd_phone,sign_in,sign_up,add_product_cart,sort_max,sort_min
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('sign_in/',sign_in),
     path('sign_up/',sign_up),
     path('add_product/',add_product_cart),
+    path('sort_max/<float:max>',sort_max),
+    path('sort_min/<float:min>',sort_min),
 ]
