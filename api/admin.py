08692ab925351c1b42df0ae51c047cc6882c5436
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-04-06 15:07:24 +0500

    init

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SmartPhone,Cart
+from .models import SmartPhone,Cart,Category
 from django.contrib.auth.models import User
 
-admin.site.register([SmartPhone,Cart])
+admin.site.register([SmartPhone,Cart,Category])
